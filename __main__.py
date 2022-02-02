4d1d72c32639287f3ede6700f2514b7367bd0670
kyle-watson <kylewatson6017@gmail.com>
2022-02-01 20:19:45 -0500

    Letter scoring system

--- __main__.py
+++ __main__.py
@@ -1,4 +1,38 @@
 
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 if __name__ == "__main__":
-    print("Hello")
+
+    # Read file
+    words = (open("allowed.txt","r").readlines())
+
+    # Trim '\n' off end of each word
+    words = [w.strip() for w in words]
+
+    # Greedy approach:
+    #   1) Make a dictonary of letters and the number of times they appear in the word list
+    #       (do not count double letters within words when scoring letters)
+    #   2) Score each word by summing each letter's score in dictionary
+    #       (also do not count double letters within words towards a words score)
+    #   3) "Guess" the word with the highest score
+    #   4) Remove guessed word if it is not the answer
+    #   5) Remove all words with black letters
+    #   6) Remove all words that do not have yellow letters
+    #   7) Remove all words with yellow letters in guessed slot
+    #   Repeat
+
+    # Future improvements
+    #   Change scoring system based on what has already been guessed?
+    
+    letter_scores = {}
+
+    for word in words:
+        # Set gives unique letters
+        for char in set(word):
+            try:
+                letter_scores[char] += 1
+            except KeyError:
+                letter_scores.update({char: 1})
+                
+    print(letter_scores)
